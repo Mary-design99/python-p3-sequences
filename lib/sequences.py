a409c665a494dd33460bd0deb0ec15d024a919d5
Mary-design99 <mary.nyarangi@student.moringaschool.com>
2025-08-06 21:57:30 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+
+    if length <= 0:
+        pass # fib_sequence is already an empty list
+    elif length == 1:
+        fib_sequence.append(0)
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < length:
+            next_fib = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_fib)
+    
+    # This line now executes for all cases, including length <= 0
+    print(fib_sequence)
+    return fib_sequence
